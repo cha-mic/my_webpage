b475aaa219fa3479e9860e50637771080f9ab5fc
cha-mic <MITIhisa0223@gmail.com>
2023-05-03 19:43:12 +0900

    add js file

---
+++ js/link.js
@@ -0,0 +1,24 @@
+
+var insta = document.getElementById("insta");
+insta.addEventListener('mouseover', function() {
+    insta.style.backgroundColor="#ffb6c1";
+}, false)
+insta.addEventListener('mouseout', function() {
+    insta.style.backgroundColor="#FFFFFF";
+}, false)
+
+var twitter = document.getElementById("twitter");
+twitter.addEventListener('mouseover', function() {
+    twitter.style.backgroundColor="#87cefa";
+}, false)
+twitter.addEventListener('mouseout', function() {
+    twitter.style.backgroundColor="#FFFFFF";
+}, false)
+
+var git = document.getElementById("git");
+git.addEventListener('mouseover', function() {
+    git.style.backgroundColor="#d3d3d3";
+}, false)
+git.addEventListener('mouseout', function() {
+    git.style.backgroundColor="#FFFFFF";
+}, false)
