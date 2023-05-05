162fd5cdf9af5df5b5928d68d38545b78517d858
cha-mic <MITIhisa0223@gmail.com>
2023-05-06 00:44:10 +0900

    modify header

--- js/head.js
+++ js/head.js
@@ -1,9 +1,13 @@
 const head = document.getElementById("headline");
 
 head.addEventListener("mouseenter", function() {
-  this.style.color = 'linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)';
+  // head.style.color = 'black';
+  // head.style.color = "linear-gradient(to right, yellow, green)";
+  head.style.backgroundImage = "linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)";
+  head.style.color = "white";
 });
 
-head.addEventListener("mouseout", function() {
-  this.style.color = "";
+head.addEventListener("mouseleave", function() {
+  head.style.backgroundImage = "";
+  head.style.color = "";
 });
